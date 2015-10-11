b5df7f505f8b83c4a71ee57c34f66c92f008b669
Matthew Carney <matthewcarney64@gmail.com>
2015-10-12 00:40:00 +0100

    Initial Release
    - Adapter Names / Status
    - Adapter Address
    - DNS Servers

--- Program.cs
+++ Program.cs
@@ -10,29 +10,20 @@ using System.Net.NetworkInformation;
 // BackgroundColors : http://www.dotnetperls.com/console-color
 // Cursor Position : http://stackoverflow.com/questions/10804233/how-to-modify-the-previous-line-of-console-text
 // DNS Information : https://msdn.microsoft.com/en-us/library/system.net.networkinformation.ipinterfaceproperties.dnsaddresses.aspx
+// IP Addresses : http://stackoverflow.com/questions/9855230/how-to-get-the-network-interface-and-its-right-ipv4-address
 
 namespace NetInfo
 {
     class Program
     {
-        static void Main(string[] args) // Seperate into Methods
+        static void Main(string[] args) // Seperate into Methods, Add Basic Pc Info EG Name updating time, ping
         {
             // Local Variable Declarations
             NetworkInterface[] localNetAdapters = NetworkInterface.GetAllNetworkInterfaces(); // Get Network Adapters
             
             // Setup Console Properties
-            System.Console.Title = "NetInfo - Version 1";    
-
-            // Get IP Addresses
-            foreach (NetworkInterface allNetworkAdapters in NetworkInterface.GetAllNetworkInterfaces())
-            {
-
-                if (allNetworkAdapters.NetworkInterfaceType == NetworkInterfaceType.Ethernet || allNetworkAdapters.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
-                {
-
-                }
-
-            }
+            System.Console.Title = "NetInfo - Version 1";
+            //System.Console.SetWindowSize(Console.WindowWidth, 100);
 
             // Get Current Adapters
             System.Console.ForegroundColor = ConsoleColor.White;
@@ -67,13 +58,46 @@ namespace NetInfo
                 }
                 
 
-                Console.WriteLine(" [" + adapter.OperationalStatus + "]"); // Get Adapter Status
+                System.Console.WriteLine(" [" + adapter.OperationalStatus + "]"); // Get Adapter Status
 
                 System.Console.ForegroundColor = ConsoleColor.Gray; // Reset Colour
 
             }
 
             System.Console.WriteLine();
+
+            // Get IP Addresses 
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine("IP Addresses....................");
+            System.Console.ForegroundColor = ConsoleColor.Gray;
+
+            foreach (NetworkInterface networkAdapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+
+                if (networkAdapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet || networkAdapter.NetworkInterfaceType == NetworkInterfaceType.Wireless80211) // If Adapter is ethernet or wireless
+                {
+
+                    System.Console.Write(networkAdapter.Name);
+
+                    foreach (UnicastIPAddressInformation adapterIP in networkAdapter.GetIPProperties().UnicastAddresses) // For Each address present on adapter
+                    {
+                        
+                        if (adapterIP.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) // Get IPv4 Addresses on adapter
+                        {
+
+                            System.Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            System.Console.WriteLine(" [" + adapterIP.Address.ToString() + "]");
+                            System.Console.ForegroundColor = ConsoleColor.Gray;
+                        
+                        }
+
+                    }
+                }
+
+            }
+
+            System.Console.WriteLine();
+
 
             // Get DNS Server Information
             System.Console.ForegroundColor = ConsoleColor.White;
